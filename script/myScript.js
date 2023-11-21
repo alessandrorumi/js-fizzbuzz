f27d7a943c1488d58af673d9b57b5592d3b3b61a
alessandrorumi <rumi.alessandro21@gmail.com>
2023-11-21 15:05:14 +0100

    Struttura JS, classi CSS per ogni elemento

--- script/myScript.js
+++ script/myScript.js
@@ -4,19 +4,26 @@ const container = document.querySelector(".container");
 // For Loop per (1 => 100)
 for (let i = 1; i <= 100; i++) {
 
-  // Assegno una variablie al contenuto e gli associo una classe (.square => CSS) ed un contenuto (i) tramite .innerHtml
-  const square= `<div class="square"> ${i} </div>`;
-  
-  // "Assegno" i div con classe .square all'html
-  container.innerHTML += square;
+  // Se la mia variabile i (del Loop) 
 
-  
-  // Se la mia variabile i (del Loop) è divisibile per 3 => "Fizz";
-  
-      // Se è divisibile per 5 => "Buzz";
-      
-      // Se è divisibile per 3 && 5 => "FizzBuzz";
-      
-      // ALtrimenti stampa il numero;
-    
+  // è divisibile per 3 && 5 => "FizzBuzz";
+  if (i % 3 === 0 && i % 5 === 0) {
+    const squareFizzBuzz= `<div class="square fizz-buzz"> ${'FizzBuzz'} </div>`;
+    container.innerHTML += squareFizzBuzz;
+
+    // è divisibile per 5 => "Buzz";
+  } else if (i % 5 === 0) {
+    const squareBuzz= `<div class="square buzz"> ${'Buzz'} </div>`;
+    container.innerHTML += squareBuzz;
+
+    // è divisibile per 3 => "Fizz"; 
+  } else if (i % 3 === 0) {
+        const squareFizz= `<div class="square fizz"> ${'Fizz'} </div>`;
+    container.innerHTML += squareFizz;
+
+    // Altrimenti stampa il numero;
+  } else {
+    const square= `<div class="square"> ${i} </div>`;
+    container.innerHTML += square;
+  }
 }
