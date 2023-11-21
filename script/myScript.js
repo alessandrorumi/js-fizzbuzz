45726396da43005d7d66bde0fa20480cbcd0d13b
alessandrorumi <rumi.alessandro21@gmail.com>
2023-11-21 14:31:19 +0100

    Creazione File, CSS macro e Istruzioni in JS

---
+++ script/myScript.js
@@ -0,0 +1,14 @@
+// Creo una variabile associata al container
+
+
+// For Loop per (1 => 100)
+
+  // Assegno una variablie al contenuto e gli associo una classe (.square => CSS) ed un contenuto (i) tramite .innerHtml
+
+    // Se la mia variabile i (del Loop) è divisibile per 3 => "Fizz";
+
+      // Se è divisibile per 5 => "Buzz";
+
+      // Se è divisibile per 3 && 5 => "FizzBuzz";
+
+      // ALtrimenti stampa il numero;
